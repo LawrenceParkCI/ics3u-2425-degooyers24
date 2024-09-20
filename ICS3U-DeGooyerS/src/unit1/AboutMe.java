74bb72393a245f6b76415d84e8f2971d340a9854
342886496 <342886496@C-MXL2393F1K.tdsb.on.ca>
2024-09-20 11:40:41 -0400

    Added AboutMe class

---
+++ ICS3U-DeGooyerS/src/unit1/AboutMe.java
@@ -0,0 +1,23 @@
+package unit1;
+/**
+ * Description: This program prints information about myself.
+ * Date: Sept. 20, 2024
+ * @author Simon de Gooyer
+ */
+
+public class AboutMe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" _______________________________________________");
+		System.out.println("|                                               |");
+		System.out.println("| INFORMATION:                                  |");
+		System.out.println("| Name == Simon                                 |");
+		System.out.println("| Grade == 11                                   |");
+		System.out.println("| Teacher == Ms Kemp                            |");
+		System.out.println("| School == Lawrence Park Collegiate Institute  |");
+		System.out.println("|_______________________________________________|");
+
+	}
+
+}
