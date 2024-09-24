5e08fa3d0c23cc98b77223deb24fe76befbc3c76
342886496 <342886496@C-MXL2393F1K.tdsb.on.ca>
2024-09-24 10:58:13 -0400

    Completed class schedule using format.

--- ICS3U-DeGooyerS/src/unit1/AboutMe.java
+++ ICS3U-DeGooyerS/src/unit1/AboutMe.java
@@ -9,18 +9,45 @@ public class AboutMe {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(" _______________________________________________");
-		System.out.println("|                                               |");
-		System.out.println("| INFORMATION:                                  |");
-		System.out.println("| Name == Simon D                               |");
-		System.out.println("| Grade == 11                                   |");
-		System.out.println("| Teacher == Ms Kemp                            |");
-		System.out.println("| School == Lawrence Park Collegiate Institute  |");
-		System.out.println("| Go Panthers!                                  |");
-		System.out.println("|                                               |");
-		System.out.format("| Class: %-15sStart Time: %-8sEnd Time: %-s     |", "ICS3U", "09:00", "10:20");
-		System.out.println("|_______________________________________________|");
-
+		System.out.println(" ______________________________________________________");
+		System.out.println("|                                                      |");
+		System.out.println("| INFORMATION:                                         |");
+		System.out.println("| Name == Simon D                                      |");
+		System.out.println("| Grade == 11                                          |");
+		System.out.println("| Teacher == Ms Kemp                                   |");
+		System.out.println("| School == Lawrence Park Collegiate Institute         |");
+		System.out.println("| Go Panthers!                                         |");
+		System.out.println("|                                                      |");
+		System.out.println("| Day 1: Monday, Friday                                |");
+		System.out.format("| Class: %-8sStart Time: %-8sEnd Time: %-8s|", "ICS3U", "09:00", "10:15");
+		System.out.println(" ");
+		System.out.format("| Class: %-8sStart Time: %-8sEnd Time: %-8s|", "FIF3U", "10:20", "11:40");
+		System.out.println(" ");
+		System.out.format("| Class: %-8sStart Time: %-8sEnd Time: %-8s|", "AMS3M", "12:40", "13:55");
+		System.out.println(" ");
+		System.out.format("| Class: %-8sStart Time: %-8sEnd Time: %-8s|", "NBE3U", "14:00", "15:15");
+		System.out.println(" ");
+		System.out.println("|                                                      |");
+		System.out.println("| Day 2: Tuesday,  Thursday                            |");
+		System.out.format("| Class: %-8sStart Time: %-8sEnd Time: %-8s|", "FIF3U", "09:00", "10:15");
+		System.out.println(" ");
+		System.out.format("| Class: %-8sStart Time: %-8sEnd Time: %-8s|", "ICS3U", "10:20", "11:40");
+		System.out.println(" ");
+		System.out.format("| Class: %-8sStart Time: %-8sEnd Time: %-8s|", "NBE3U", "12:40", "13:55");
+		System.out.println(" ");
+		System.out.format("| Class: %-8sStart Time: %-8sEnd Time: %-8s|", "AMS3M", "14:00", "15:15");
+		System.out.println(" ");
+		System.out.println("|                                                      |");
+		System.out.println("| Late Start: Wednesday                                |");
+		System.out.format("| Class: %-8sStart Time: %-8sEnd Time: %-8s|", "ICS3U", "09:55", "11:00");
+		System.out.println(" ");
+		System.out.format("| Class: %-8sStart Time: %-8sEnd Time: %-8s|", "FIF3U", "11:05", "12:10");
+		System.out.println(" ");
+		System.out.format("| Class: %-8sStart Time: %-8sEnd Time: %-8s|", "AMS3M", "13:10", "14:10");
+		System.out.println(" ");
+		System.out.format("| Class: %-8sStart Time: %-8sEnd Time: %-8s|", "NBE3U", "14:15", "15:15");
+		System.out.println(" ");
+		System.out.println("|______________________________________________________|");
 	}
 
 }
