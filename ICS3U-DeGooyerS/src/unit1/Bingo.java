b5147f8a1c6cd4cedb6b1fd8e6db26e7280a90e2
342886496 <342886496@C-MXL2393F1K.tdsb.on.ca>
2024-09-24 11:37:06 -0400

    Created Bingo class

---
+++ ICS3U-DeGooyerS/src/unit1/Bingo.java
@@ -0,0 +1,31 @@
+package unit1;
+
+public class Bingo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1. The caller randomly pulls a numbered bingo ball.");
+		System.out.println(" ");
+		System.out.println("2. The number is placed on the bingo board and called out.");
+		System.out.println(" ");
+		System.out.println("3. Players look for the called number on their bingo card.");
+		System.out.println(" ");
+		System.out.println("4. If the number is located, it is marked off.");
+		System.out.println(" ");
+		System.out.println("5. Steps 1 to 4 are repeated until a player matches the BINGO pattern");
+		System.out.println(" ");
+		System.out.println("6. The winning player yells BINGO.");
+		System.out.println(" ");
+		System.out.printf("%8s%8s%8s%8s%8s", "B", "I", "N", "G", "O");
+		System.out.println(" ");
+		System.out.println(" ");
+		System.out.printf("|%8s%8s%8s%8s%8s|", "12", "27", "32", "46", "71");
+		System.out.println(" ");
+		System.out.println(" ");
+		System.out.printf("|%8s%8s%8s%8s%8s|", "3", "17", "41", "54", "68");
+		System.out.println(" ");
+		System.out.println(" ");
+		System.out.printf("|%8s%8s%8s%8s%8s|", "7", "23", "FREE", "44", "62");
+	}
+
+}
