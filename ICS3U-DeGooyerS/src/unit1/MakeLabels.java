3b588982a195a0be76862ec51e39ca8d19a8b715
342886496 <342886496@C-MXL2393F1K.tdsb.on.ca>
2024-10-02 10:59:53 -0400

    Added MakeLabels class

---
+++ ICS3U-DeGooyerS/src/unit1/MakeLabels.java
@@ -0,0 +1,35 @@
+package unit1;
+/**
+ * Description: This program takes the user's name from an input in the console, as well as their subject and prints it.
+ * Date: Oct. 2, 2024
+ * @author Simon de Gooyer
+ */
+
+import java.util.Scanner;
+
+public class MakeLabels {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+		
+		String name;
+		
+		System.out.println("Type in your name and press <Enter>");
+		name = sc.nextLine();
+		
+		String subject;
+		
+		System.out.println("Type in the subject and press <Enter>");
+		subject = sc.nextLine();
+		
+		System.out.println();
+		System.out.println("******************************");
+		System.out.println(name);
+		System.out.println(subject);
+		System.out.println("******************************");
+		
+
+	}
+
+}
