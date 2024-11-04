1c53c14efaf4dd68af23c5453b0a830ba23fd39d
342886496 <342886496@C-MXL2393F1K.tdsb.on.ca>
2024-11-04 11:20:31 -0500

    Added basic boolean

--- ICS3U-DeGooyerS/src/unit2/OddEven.java
+++ ICS3U-DeGooyerS/src/unit2/OddEven.java
@@ -8,15 +8,20 @@ import java.util.Scanner;
  * @author Simon de Gooyer
  */
 public class OddEven {
+	/**
+	 * This is the entry point to the program
+	 * @param args unused
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		//This creates scanner object
 		Scanner sc = new Scanner(System.in);
 		
+		//Taking user input, declaring and setting variable
 		System.out.println("Enter a whole number.");
 		int number = sc.nextInt();
 		
+		//If statements decide what to print.
 		number = number % 2;
 		if(number == 0) {
 			System.out.println("That number is even.");
--- ICS3U-DeGooyerS/src/unit2/PositiveNegative.java
+++ ICS3U-DeGooyerS/src/unit2/PositiveNegative.java
@@ -8,23 +8,29 @@ import java.util.Scanner;
  * @author Simon de Gooyer
  */
 public class PositiveNegative {
+	
+	/**
+	 * This is the entry point to the program
+	 * @param args unused
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		//Creating scanner object
 		Scanner sc = new Scanner(System.in);
 		
+		//Taking user input, declaring and setting variable
 		System.out.println("Enter an integer.");
 		int number = sc.nextInt();
 		
+		//If statements determine positive or negative
 		if (number >= 0) {
 			System.out.println("This number is positive.");
 		}
 		else {
 			System.out.println("This number is negative");
 		}
-		number = number % 7;
-		if (number == 0) {
+		//If statements determine divisibility by 7.
+		if (number % 7 == 0) {
 			System.out.println("This number is divisible by 7.");
 		}
 		else {
