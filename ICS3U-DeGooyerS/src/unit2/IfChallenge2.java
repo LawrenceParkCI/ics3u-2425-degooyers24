ac5830e8e12867273b368724419309dd291745b8
342886496 <342886496@C-MXL2393F1K.tdsb.on.ca>
2024-11-05 10:13:54 -0500

    Fixed IfChallenge2

--- ICS3U-DeGooyerS/src/unit2/IfChallenge2.java
+++ ICS3U-DeGooyerS/src/unit2/IfChallenge2.java
@@ -1,10 +1,39 @@
 package unit2;
 
+import java.util.Scanner;
+
+/**
+ * Description: This is a challenge program for if statements.
+ * Date: Nov. 5, 2024
+ * @author Simon de Gooyer
+ */
+
 public class IfChallenge2 {
+	
+	/**
+	 * This is the entry point to the program.
+	 * @param args unused
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		 //This is for the portfolio
 
+	    /*Create a program that asks for three numbers.
+	    Tell the user if the numbers are strictly in order.
+	    ie.  2 5 11 or 5 6 7 are strictly in order.
+	    ie.  6 5 7 or 5 5 7 are not
+	    */
+		System.out.println("Enter an integer");
+		int num1 = sc.nextInt();
+		System.out.println("Enter another integer");
+		int num2 = sc.nextInt();
+		System.out.println("Enter a final integer");
+		int num3 = sc.nextInt();
+		
+		
+		sc.close();
+		
 	}
 
 }
