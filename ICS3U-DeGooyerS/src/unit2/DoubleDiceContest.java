fc95d00b75409ec6c569c3a2c6118405cf0fbefa
342886496 <342886496@C-MXL2393F1K.tdsb.on.ca>
2024-11-18 12:11:16 -0500

    Added DoubleDiceContest class

---
+++ ICS3U-DeGooyerS/src/unit2/DoubleDiceContest.java
@@ -0,0 +1,16 @@
+package unit2;
+
+public class DoubleDiceContest {
+
+	public static void main(String[] args) {
+		  /*
+		   * Write code that simulates rolling two dice for the user and then two dice for the computer. 
+		   * The program should output the total of the two dice for both the user and the computer, 
+		   * then announce who won: the computer, the user, or say it was a tie.
+		   */
+
+
+
+	}
+
+}
