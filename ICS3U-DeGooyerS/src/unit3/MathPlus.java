27475e13880b457be72698ecde861f7152bc5d70
342886496 <342886496@C-MXL2393F1K.tdsb.on.ca>
2024-12-19 10:17:05 -0500

    Worked on MathPlus

--- ICS3U-DeGooyerS/src/unit3/MathPlus.java
+++ ICS3U-DeGooyerS/src/unit3/MathPlus.java
@@ -62,6 +62,53 @@ public class MathPlus {
 			System.out.println(potentialPrime + " is not a prime number.");
 		}
 		
+		System.out.println("Please enter 5 integers, and press <Enter> after each.");
+		int[] array1;
+		array1 = new int[5];
+		array1[0] = sc.nextInt();
+		array1[1] = sc.nextInt();
+		array1[2] = sc.nextInt();
+		array1[3] = sc.nextInt();
+		array1[4] = sc.nextInt();
+		System.out.println("The sum of these numbers is " + sum(array1));
+		
+		System.out.println("Please enter 5 doubles, and press <Enter> after each.");
+		double[] array2;
+		array2 = new double[5];
+		array2[0] = sc.nextDouble();
+		array2[1] = sc.nextDouble();
+		array2[2] = sc.nextDouble();
+		array2[3] = sc.nextDouble();
+		array2[4] = sc.nextDouble();
+		System.out.println("The sum of these numbers is " + sum(array2));
+		
+		System.out.println("Please enter 5 integers, and press <Enter> after each.");
+		int[] array3;
+		array3 = new int[5];
+		array3[0] = sc.nextInt();
+		array3[1] = sc.nextInt();
+		array3[2] = sc.nextInt();
+		array3[3] = sc.nextInt();
+		array3[4] = sc.nextInt();
+		System.out.println("The difference between the largest and smallest numbers is " + bigDifference(array3));
+		
+		System.out.println("Please enter 5 doubles, and press <Enter> after each.");
+		double[] array4;
+		array4 = new double[5];
+		array4[0] = sc.nextDouble();
+		array4[1] = sc.nextDouble();
+		array4[2] = sc.nextDouble();
+		array4[3] = sc.nextDouble();
+		array4[4] = sc.nextDouble();
+		System.out.println("The difference between the largest and smallest numbers is " + bigDifference(array4));
+		
+		System.out.println("Please enter an integer.");
+		int factoredNumber = sc.nextInt();
+		System.out.println("The factors of this number are:");
+		int[] factors = factors(factoredNumber);
+		for (int i = 0; i < factors.length; i++) {
+			System.out.println(factors[i]);
+		}
 		sc.close();
 	}
 	
@@ -122,12 +169,130 @@ public class MathPlus {
 				factorCount++;
 			}
 		}
-		if (factorCount != 2) {
+		if (factorCount <= 2) {
 			isPrime = false;
 		}
 		return isPrime;
 	}
 	
+	/**
+	 * This method returns the sum of the integers in the array it is given
+	 * @param array the array containing the numbers
+	 * @return the sum of array
+	 */
+	
+	public static int sum (int[] array) {
+		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum = sum + array[i];
+		}
+		return sum;
+	}
+	
+	/**
+	 * This method returns the sum of the doubles in the array it is given
+	 * @param array the array containing the doubles
+	 * @return the sum of array
+	 */
+	
+	public static double sum (double[] array) {
+		double sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum = sum + array[1];
+		}
+		return sum;
+	}
+	
+	public static int min (int[] array) {
+		int min = array[0];
+		int minIndex = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] < min) {
+				min = array[i];
+				minIndex = i;
+			}
+		}
+		return minIndex;
+	}
+	
+	/**
+	 * This method returns the difference between the max and min ints
+	 * @param array the array containing the numbers
+	 * @return difference between the largest and smallest numbers
+	 */
+	
+	public static int bigDifference (int[] array) {
+		int max = array[0];
+		int min = array[0];
+		
+		int difference;
+		
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] > max) {
+				max = array[i];
+			}
+		}
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] < min) {
+				min = array[i];
+			}
+		}
+		
+		difference = max - min;
+		return difference;
+	}
+	
+	/**
+	 * This method returns the difference between max and min doubles
+	 * @param array contains numbers to be checked
+	 * @return the difference between the max and min value
+	 */
+	
+	public static double bigDifference (double[] array) {
+		double max = array[0];
+		double min = array[0];
+		
+		double difference;
+		
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] > max) {
+				max = array[i];
+			}
+		}
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] < min) {
+				min = array[i];
+			}
+		}
+		
+		difference = max - min;
+		return difference;
+	}
+	
+	/**
+	 * This method returns an array containing the factors of a number.
+	 * @param number the number whose factors are printed
+	 * @return the array containing the factors
+	 */
+	
+	public static int[] factors (int number) {
+		int factorCount = 0;
+		for (int i = 1; i <= number; ++i) {
+			if (number % i == 0) {
+				factorCount++;
+			}
+		}
+		int[] array;
+		int arrayPosition = 0;
+		array = new int[factorCount];
+		for (int i = 1; i <= number; i++) {
+			if (number % i == 0) {
+				array[arrayPosition] = i;
+				arrayPosition++;
+			}
+		}
+		return array;
+	}
 	
 
 }
