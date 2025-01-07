014d3458ad5ff82402c63249157b262feeaedcb9
342886496 <342886496@C-MXL2393F1K.tdsb.on.ca>
2025-01-07 10:15:48 -0500

    Added hangman class

--- ICS3U-DeGooyerS/src/finalProject/Hangman.java
+++ ICS3U-DeGooyerS/src/finalProject/Hangman.java
@@ -1,9 +1,15 @@
 package finalProject;
 
+import java.util.Scanner;
+
 public class Hangman {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		sc.close();
 
 	}
 
